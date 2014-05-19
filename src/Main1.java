97782a8d357a3c3c6ceaa1808eaf5a89ae99e356
MikeSavenkov <savenkovmike@gmail.com>
2014-05-20 01:07:56 +0400

    сделали пункт 1.4
    надо пофиксить нолики в выводе

--- src/Main1.java
+++ src/Main1.java
@@ -2,7 +2,8 @@
  * Created by mikesavenkov on 4/21/14.
  */
 
-import static java.lang.System.*;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
 
 /**
  * Курсовая работа: Системы счисления и запись целых чисел в памяти компьютера
@@ -20,7 +21,11 @@ public class Main1 {
         //secondVersion(7);
         //System.out.println(plus(7, 12));
         //System.out.println(minus(10, 4));
-        System.out.println(multiply(9, 6));
+        //System.out.println(multiply(9, 6));
+        //System.out.println(dop_9(-35));
+        //dop_9(-34) ;
+        plusDop_9(34, 523);                    //01-091
+        //minusDop_9(53, 7);
 
     }
 
@@ -79,18 +84,60 @@ public class Main1 {
     public static int multiply(int a, int b) {
         int i=0, c=0;
         while(b!=0){
-          while ((b & 1) == 0) {
-             i++;
-             b = b >> 1;
-          }
-
-          a=a<<i;
-          c=plus(c, a);
-          b=b>>1;
-          i=1;
-
+            while ((b & 1) == 0) {
+                i++;
+                b = b >> 1;
+            }
+            a=a<<i;
+            c=plus(c, a);
+            b=b>>1;
+            i=1;
         }
         return c;
     }
+
+    public static Integer dop_9(int a){
+        int b = 0;
+        int c,g;
+        String temp = "";
+        if (a==0)
+            System.out.print(0);
+        else{
+            if(a>0){
+                b = b>>>1;
+                System.out.print(b);
+            }
+            else{
+                a = abs(a);
+                  while (a != 0){
+                    c = a%10;
+                    a = a/10;
+                    g = 9-c;
+                    temp = g + temp;
+                  }
+                  System.out.println("9"+temp);
+            }
+
+        }
+
+        return a;
+    }
+    public static void plusDop_9 (int a, int b){
+        int c;
+        dop_9(a);
+        dop_9(b);
+        c = a + b;
+        System.out.println(c);
+
+    }
+    public static void minusDop_9 (int a, int b){
+        int c;
+        dop_9(a);
+        dop_9(b);
+        c = a - b;
+
+        System.out.println(dop_9(c));
+    }
+
 }
 
